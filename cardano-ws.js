60c8910d64f84b09c27597715a39a6a78a2cd3cb
Kenneth Phang <bunnyppl@gmail.com>
2018-05-01 11:07:04 +0800

    chore: refactor files

--- cardano-ws.js
+++ cardano-ws.js
@@ -7,18 +7,17 @@ const url = require('url');
 const WebSocket = require('ws');
 const util = require('util');
 const bs58 = require('bs58');
-const config = require('./config');
-const Util = require('./util');
-const logger = require('./util/logger');
-
+const os = require('os');
+var networkInterface = os.networkInterfaces();
+console.log(networkInterface);
 const app = express();
-var b64c = config.cardanoHexRand;
-var utils = new Util();
+var b64c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 var options = {
-    hostname: config.cardanoWSAddress,
-    port: config.cardanoWSPort,
-    key:   fs.readFileSync(config.cardanoCertificateKey),  // Secret client key
-    cert:  fs.readFileSync(config.cardanoCertificateCert),
+    hostname: 'localhost',
+    port: 8090,
+    key:   fs.readFileSync('client.key'),  // Secret client key
+    cert:  fs.readFileSync('client.crt'),
 };
 
 app.use(function (req, res) {
@@ -26,71 +25,106 @@ app.use(function (req, res) {
 });
 
 const server = http.createServer(app);
-server.timeout = 0;
-const wss = new WebSocket.Server({ server });
+server.perMessageDeflate = {
+        zlibDeflateOptions: { // See zlib defaults.
+          chunkSize: 1024,
+          memLevel: 7,
+          level: 3,
+        },
+        zlibInflateOptions: {
+          chunkSize: 10 * 1024
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        clientMaxWindowBits: 10,       // Defaults to negotiated value.
+        serverMaxWindowBits: 10,       // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10,          // Limits zlib concurrency for perf.
+        threshold: 1024,               // Size (in bytes) below which messages
+                                       // should not be compressed.
+};
+var wss = new WebSocket.Server({ server });
 
+function noop() {}
+
+function heartbeat(){
+    this.Alive = true;
+}
 wss.on('connection', function connection(ws, req) {
+  const location = url.parse(req.url, true);
+  ws.isalive = true;
+
+  ws.on('pong', heartbeat);
+
+  // You might use location.query.access_token to authenticate or share sessions
+  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+   console.log("Connected!");
   ws.on('error', function(error) {
-    logger.error('errored' + error);
-    if (error.errno) return;
-    throw error;
+      console.log('errored' + error);
+      if (error.errno) return;
+      throw error;
   });
 
   ws.on('close', function(){
-    logger.info("client closed connection!");
+      console.log("client closed connection!");
   });
 
   ws.on('message', function incoming(message) {
-    logger.debug('received: %s', message);
+    console.log('received: %s', message);
     var txnMessage = JSON.parse(message);
 
       if(txnMessage.type === 'accounts'){
-          logger.debug("accounts ...");
+          console.log("accounts ...");
           options.method = 'GET';
           options.path = '/api/accounts';
-          logger.debug("accounts ...");
+          console.log("accounts ...");
           var accountsReq = https.request(options, function(res) {
-              logger.debug("res"  + res);
+              console.log("res"  + res);
               res.on('data', function(data) {
                   process.stdout.write(data);
-                  logger.debug('\n');
+                  console.log('\n');
                   var result = JSON.parse(data);
               });
           });
           accountsReq.end();
       } else if(txnMessage.type === 'wallets'){
-        logger.debug("wallets ...");
+        //console.log("wallets ...");
         options.method = 'GET';
         options.path = '/api/wallets';
-        logger.debug("wallets ...");
+        //console.log("wallets ...");
         var walletsReq = https.request(options, function(res) {
-            logger.debug("res"  + res);
+            //console.log("res"  + res);
             res.on('data', function(data) {
-                process.stdout.write(data);
-                logger.debug('\n');
-                var result = JSON.parse(data);
+                //process.stdout.write(data);
+                data.txnMessage = txnMessage;
+                //var result = _parseJSON(data);
+                console.log('\n'  + data);
+                ws.send(JSON.stringify(data));
             });
         });
         walletsReq.end();
     } else if(txnMessage.type === 'balance'){
-      logger.debug("balance ...");
+      console.log("balance ...");
       options.method = 'GET';
       options.path = '/api/wallets/' + txnMessage.walletId;
-      logger.debug("before ...");
+      console.log("before ...");
       var balreq = https.request(options, function(res) {
-          logger.debug("res"  + res);
+          console.log("res"  + res);
           res.on('data', function(data) {
               process.stdout.write(data);
-              logger.debug('\n');
+              console.log('\n');
               var result = JSON.parse(data);
-              logger.debug(result.Right.cwAmount.getCCoin);
+              console.log(result.Right.cwAmount.getCCoin);
+              result.txnMessage = txnMessage;
+              ws.send(JSON.stringify(result));
           });
       });
       balreq.end();
     } else if( txnMessage.type === 'new') {
         var mnemonic = bip39.generateMnemonic();
         var bpListArr = mnemonic.split(' ');
-        logger.debug(bpListArr);
+        console.log(bpListArr);
         var postData = {
             "cwInitMeta": {
                 "cwName": "test1",
@@ -103,11 +137,11 @@ wss.on('connection', function connection(ws, req) {
         }
         options.method = 'POST';
 
-        var encodedPassphrase = utils.toHexBase16(txnMessage.passphrase);
+        var encodedPassphrase = toHexBase16(txnMessage.passphrase);
 
         options.path = '/api/wallets/new?passphrase=' + encodedPassphrase;
-        logger.debug(options.path);
-        logger.debug(JSON.stringify(postData).length);
+        console.log(options.path);
+        console.log(JSON.stringify(postData).length);
         options.headers = {
             'Content-Type': 'application/json',
             'Content-Length': JSON.stringify(postData).length
@@ -115,10 +149,10 @@ wss.on('connection', function connection(ws, req) {
         options.json = true;
         options.body = JSON.stringify(postData);
 
-        submitRequest(options, postData, ws, txnMessage.email);
+        submitRequest(options, postData, ws, txnMessage);
 
     } else if( txnMessage.type === 'transfer') {
-        logger.debug('transfer --- ');
+        console.log('transfer --- ');
           var postData = {"groupingPolicy": "OptimizeForSecurity"};
 
           options.method = 'POST';
@@ -126,21 +160,20 @@ wss.on('connection', function connection(ws, req) {
           var fromAddress = txnMessage.fromAddress;
           var toAddress = txnMessage.toAddress;
           var amount = txnMessage.amount;
-          var encodedPassphrase = utils.toHexBase16(txnMessage.passphrase);
+          var encodedPassphrase = toHexBase16(txnMessage.passphrase);
           options.path = '/api/txs/payments/'+ fromAddress +'/' + toAddress + '/' + amount
               + '?passphrase=' + encodedPassphrase;
-          logger.debug(options.path);
-          logger.debug(JSON.stringify(postData).length);
+          console.log(options.path);
+          console.log(JSON.stringify(postData).length);
           options.headers = {
               'Content-Type': 'application/json',
               'Content-Length': JSON.stringify(postData).length
           };
           options.json = true;
           options.body = JSON.stringify(postData);
-          submitRequest(options, postData, ws, txnMessage.email);
-
+          submitRequest(options, postData, ws, txnMessage);
      } else if (txnMessage.type === 'fee'){
-        logger.debug('fee --- ');
+        console.log('fee --- ');
         var postData = {"groupingPolicy": "OptimizeForSecurity"};
 
         options.method = 'POST';
@@ -150,34 +183,56 @@ wss.on('connection', function connection(ws, req) {
         var amount = txnMessage.amount;
 
         options.path = '/api/txs/fee/'+ fromAddress +'/' + toAddress + '/' + amount;
-        logger.debug(options.path);
-        logger.debug(JSON.stringify(postData).length);
+        console.log(options.path);
+        console.log(JSON.stringify(postData).length);
         options.headers = {
             'Content-Type': 'application/json',
             'Content-Length': JSON.stringify(postData).length
         };
         options.json = true;
         options.body = JSON.stringify(postData);
-         submitRequest(options, postData, ws, txnMessage.email);
+        submitRequest(options, postData, ws, txnMessage);
     }
 
   });
 
 });
 
+const interval = setInterval(function ping(){
+    wss.clients.forEach(function each(ws){
+        if(ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping(noop);
+    })
+}, 30000);
+
 server.listen(8080, function listening() {
-  logger.debug('Cardano Engine Listening on %d', server.address().port);
+    console.log('Cardano Engine Server %s', JSON.stringify(server.address()));
+    console.log('Cardano Engine Listening on %d', server.address().port);
 });
 
-function submitRequest(options, postData, _ws, emailAddress){
+function toHexBase16(s) {
+    // utf8 to latin1
+    var s = unescape(encodeURIComponent(s))
+    var h = ''
+    for (var i = 0; i < s.length; i++) {
+        h += s.charCodeAt(i).toString(16)
+    }
+    return h
+}
+
+function submitRequest(options, postData, _ws, _txnMessage){
     var newAccReq = https.request(options, function (res) {
         res.on('data', function(data) {
+            console.log('! !');
             process.stdout.write("???>>>" + data);
-            logger.debug("sending back to the client ...");
+            console.log("sending back to the client ...");
             var _result = JSON.parse(data);
             postData.result = _result;
-            postData.email = emailAddress;
-            logger.debug(JSON.stringify(postData));
+            delete _txnMessage.passphrase;
+            postData.txnMessage = _txnMessage;
+            console.log(JSON.stringify(postData));
             _ws.send(JSON.stringify(postData));
         });
     });
@@ -188,5 +243,4 @@ function submitRequest(options, postData, _ws, emailAddress){
 
     newAccReq.write(JSON.stringify(postData), encoding = 'utf8');
     newAccReq.end();
-
 }
--- startEngine.cmd
+++ startEngine.cmd
@@ -1,2 +1,2 @@
 set NODE_TLS_REJECT_UNAUTHORIZED=0
-nodemon ../cardano-ws.js
+nodemon app.js
