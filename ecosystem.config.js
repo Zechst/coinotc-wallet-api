e35fb6f15c9e207357ab12e21f5a88287d88c3c4
Kenneth Phang <bunnyppl@gmail.com>
2018-07-02 14:14:52 +0800

    feat: pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    {
+      name      : 'WalletAPI',
+      script    : 'app.js',
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    {
+      name      : 'RippleWS',
+      script    : 'ripple-ws.js'
+    },
+
+    {
+      name      : 'StellarWS',
+      script    : 'stellar-ws.js'
+    },
+
+    {
+      name      : 'CardanoWS',
+      env: {
+        NODE_TLS_REJECT_UNAUTHORIZED: 0,
+      },
+      script    : 'cardano-ws.js'
+    },
+  ],
+};
